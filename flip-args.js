a577463a5e39be73eaeb1c38f90bddf21c6801bc
anu8280 <vivsharma2002@gmail.com>
2021-01-08 11:06:13 +0530

    commit 7-11-20 work

---
+++ flip-args.js
@@ -0,0 +1,11 @@
+function clickResponse() {
+
+var arg1, arg2
+
+arg1 = document.getElementById("arg1").value
+arg2 = document.getElementById("arg2").value
+console.table("Before flipping, the first argument is: " + arg1)
+console.table("Before flipping, the second argument is: " + arg2)
+
+
+}
