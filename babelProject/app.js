121b875401615d1ac0effd4a18f1b02b0c36394b
Vivek <vivsharma2002@gmail.com>
2021-01-21 12:03:03 +0530

    uploaded babelproject for classes with Webpack

---
+++ babelProject/app.js
@@ -0,0 +1,11 @@
+// import Student class
+
+import Student from './components/Student'
+
+const student1 = new Student()
+
+student1.addStudent()
+
+student1.listStudents()
+
+student1.getStudentById()
---
+++ babelProject/components/Student.js
@@ -0,0 +1,22 @@
+class Student
+{
+
+    // class functions/methods
+    addStudent()
+    {
+        console.log("Add Student function")
+    }
+
+    listStudents()
+    {
+        console.log("List Student Function")
+    }
+
+    getStudentById()
+    {
+        console.log("Get student by id function")
+    }
+}
+
+
+export default Student
