8a20fbb76c1def3deb150625bc73fc478cb3ec08
Vivek <vivsharma2002@gmail.com>
2021-01-25 12:12:02 +0530

    Babel project form

---
+++ babeltestproject/app.js
@@ -0,0 +1,7 @@
+import Student from "./components/Student"
+
+const student1 = new Student()
+
+student1.addStudents()
+student1.listStudents()
+student1.getStudentsById()
---
+++ babeltestproject/components/Student.js
@@ -0,0 +1,15 @@
+class Student{
+    addStudents(){
+        console.log("adding students")
+    }
+
+    listStudents(){
+        console.log("listing out students")
+    }
+
+    getStudentsById(){
+        console.log("getting students by their IDs")
+    }
+}
+
+export default Student
