15d59911d46e19f7519834e102655cab25bc101f
anu8280 <vivsharma2002@gmail.com>
2021-03-04 11:52:59 +0530

    authentication process

--- backendNode/routes/users.js
+++ backendNode/routes/users.js
@@ -5,17 +5,69 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 const saltRounds = 10;
 
+// authentication middleware
 function authentication(req, res, next) {
     const authHeader = req?.headers?.authorization || null;
+    let refreshToken = req?.headers?.token || null;
     if (authHeader == null || authHeader === "") { return res.status(401).json({ status: false, message: "Unauthorized Access" }); }
 
     let token = JSON.parse(authHeader)
 
-    jwt.verify(token, 'BACKEND_NODE', (err, user) => {
+    jwt.verify(token, 'BACKEND_NODE', async (err, user) => {
         if (err) {
-            return res.status(401).json({ status: false, message: "Token is expired, please login again." });
+            switch (err.message) {
+                case 'jwt expired':
+
+                    if (refreshToken) {
+
+                        refreshToken = JSON.parse(refreshToken)
+
+                        // fetch refreshTOken from DB
+                        const dbUser = await Users.findOne({ refreshToken })
+
+                        if (!dbUser) {
+
+                            return res.status(401).json({ status: false, message: "Invalid Refresh Token, please login again." });
+
+                        }
+
+
+                        jwt.verify(refreshToken, 'BACKEND_NODE', (err1, user1) => {
+                            if (err1) {
+                                return res.status(401).json({ status: false, message: "Refresh Token is expired, please login again." });
+                            }
+
+
+                            // new token generation
+                            const userData = { _id: user1._id, name: user1.name, email: user1.email, phone: user1.phone };
+                            const accessToken = jwt.sign(userData, 'BACKEND_NODE', { expiresIn: "15000" })
+
+                            res.user = {
+                                accessToken,
+                                ...userData,
+                            };
+
+                            next();
+
+                            return
+
+
+                        })
+
+
+                    }
+
+                    break;
+                case 'invalid token':
+                    return res.status(401).json({ status: false, message: "Sorry, You have provided invalid token." });
+                    break;
+                default:
+                    return res.status(401).json({ status: false, message: "Token is a problem with the token." });
+
+            }
+
         } else {
-            res.locals.user = {
+            res.user = {
                 accessToken: token,
                 ...user,
             };
@@ -48,7 +100,7 @@ router.post('/register', async function (req, res) {
         })
 
     } else {
-        res.status(500).json({ status: false, message: "Please fill the rquired fields" })
+        res.status(500).json({ status: false, message: "Please fill the required fields" })
     }
 
 })
@@ -77,8 +129,8 @@ router.post('/login', async function (req, res) {
         // valid user and generate token
 
         const userData = { _id: user._id, name: user.name, email: user.email, phone: user.phone };
-        const accessToken = jwt.sign(userData, 'BACKEND_NODE', { expiresIn: "600000" })
-        const refreshToken = jwt.sign(userData, 'BACKEND_NODE', { expiresIn: "1800000" })
+        const accessToken = jwt.sign(userData, 'BACKEND_NODE', { expiresIn: "15000" })
+        const refreshToken = jwt.sign(userData, 'BACKEND_NODE', { expiresIn: "1d" })
 
         await Users.updateOne({ _id: user._id }, {
             $set: {
@@ -111,10 +163,11 @@ router.post('/login', async function (req, res) {
 // protected api
 router.post('/get-user', authentication, async function (req, res) {
 
-    const { _id } = req.body
+    const { _id } = res.user
     if (_id) {
 
         const user = await Users.findOne({ _id })
+
         res.status(200).json({ status: true, user: { name: user.name, email: user.email, phone: user.phone, date: user.date } })
 
     }
